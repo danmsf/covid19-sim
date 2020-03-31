fd0b1f5248e723bb0e56072a5dadd99d01351504
Dan <d@d.com>
2020-03-31 11:20:31 +0300

    changes

--- SimCode/SEIR_MCMC/sir_model.py
+++ SimCode/SEIR_MCMC/sir_model.py
@@ -7,50 +7,41 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 import arviz as az
 from models import SIR
+import os
 import theano
+os.environ["THEANO_FLAGS"] = "device=cuda"
 
-# S, I, R = y
-# dSdt = -beta * S * I / N
-# dIdt = beta * S * I / N - gamma * I
-# dRdt = gamma * I
-# F = beta * I ~ Force of infection; can also be modeleld as beta * I/N
-# def SIR(y, t, p):
-#     ds = -p[0]*y[0]*y[1]
-#     di = p[0]*y[0]*y[1] - p[1]*y[1]
-#     dr = p[1] * y[1]
-#     return [ds, di, dr]
-
-times = np.arange(0,5,0.25)
-times = np.linspace(0, 160, 160)
+times = np.arange(0, 5, 0.05)
+# times = np.arange(0, 160, 1)
+# times = np.linspace(0, 160, 160)
 # 1/gamma = time infectious ~ 14 ; or gamma = 1/D = 1/14
 # beta*Nt = number of people in contact with infected person at time t; S/N of them are susceptible to contagion
 # R0 = beta/gamma
 
 beta, gamma = 4, 1.0
-beta, gamma = 0.25, 1./14
+beta, gamma = 0.25, 1.0/14
+beta, gamma = 1./0.25, 1.14
 # Create true curves
 # y0 = [S/N, I]
-y = odeint(SIR, t=times, y0=[999, 1, 0.0], args=((beta, gamma),), rtol=1e-8)
+y = odeint(SIR, t=times, y0=[999.0/1000, 1.0/1000, 0.0], args=((beta, gamma),), rtol=1e-8)
 # Observational model.  Lognormal likelihood isn't appropriate, but we'll do it anyway
 yobs = np.random.lognormal(mean=np.log(y[1::]), sigma=[0.2, 0.3, 0.2])
 
 plt.figure()
 # plt.plot(times[1::], yobs, marker='o', linestyle='none')
-plt.plot(times, y[:, 0]/1000, color='C0', alpha=0.5, label=f'$S(t)$')
-plt.plot(times, y[:, 1]/1000, color='C1', alpha=0.5, label=f'$I(t)$')
-plt.plot(times, y[:, 2]/1000, color='C2', alpha=0.5, label=f'$R(t)$')
+plt.plot(times, y[:, 0], color='C0', alpha=0.5, label=f'$S(t)$')
+plt.plot(times, y[:, 1], color='C1', alpha=0.5, label=f'$I(t)$')
+plt.plot(times, y[:, 2], color='C2', alpha=0.5, label=f'$R(t)$')
 plt.legend()
 plt.title(f'R0={beta/gamma:.2f}; $\\beta$={beta}; $\\gamma$={gamma:.2f}; d={14}')
 plt.show()
 
 
 
-yobs = np.random.lognormal(mean=np.log(y/1000), sigma=[0.2, 0.3, 0.2])
-
 
 sir_model = DifferentialEquation(
     func=SIR,
-    times=np.linspace(0, 160, 160),
+    times=np.arange(0.05, 5, 0.05),
     n_states=3,
     n_theta=2,
     t0=0,
@@ -64,61 +55,37 @@ with pm.Model() as model4:
     lam = pm.Lognormal('lambda', pm.math.log(2), 2)
     beta = pm.Deterministic('beta', lam*R0)
 
-    sir_curves = sir_model(y0=[999/1000, 1/1000, 0.0], theta=[beta, lam])
+    sir_curves = sir_model(y0=[999.0/1000, 1.0/1000, 0.0], theta=[beta, lam])
 
     Y = pm.Lognormal('Y', mu=pm.math.log(sir_curves), sd=sigma, observed=yobs)
 
     prior = pm.sample_prior_predictive()
     # step = pm.Metropolis()
     # trace = pm.sample(2000, tune=1000, target_accept=0.9, cores=1, step=step)
-    trace = pm.sample(20, tune=10, target_accept=0.9, cores=1)
+    trace = pm.sample(500, tune=500, target_accept=0.9, cores=1, start={'lamda': 1.2, 'beta': 1./0.25})
     posterior_predictive = pm.sample_posterior_predictive(trace)
 
     data = az.from_pymc3(trace=trace, prior=prior, posterior_predictive=posterior_predictive)
 
-
-
-
-
-
-
-
-
-
-# def SIR(y, t, p):
-#     ds = -p[0]*y[0]*y[1]
-#     di = p[0]*y[0]*y[1] - p[1]*y[1]
-#     dr = p[1] * y[1]
-#     return [ds, di, dr]
-
-sir_model = DifferentialEquation(
-    func=SIR,
-    times=np.arange(0.25, 5, 0.25),
-    n_states=2,
-    n_theta=2,
-    t0=0,
-)
-
-with pm.Model() as model4:
-    sigma = pm.HalfCauchy('sigma', 1, shape=2)
-
-    # R0 is bounded below by 1 because we see an epidemic has occurred
-    R0 = pm.Bound(pm.Normal, lower=1)('R0', 2, 3)
-    lam = pm.Lognormal('lambda', pm.math.log(2), 2)
-    beta = pm.Deterministic('beta', lam*R0)
-
-    sir_curves = sir_model(y0=[0.99, 0.01], theta=[beta, lam])
-
-    Y = pm.Lognormal('Y', mu=pm.math.log(sir_curves), sd=sigma, observed=yobs)
-
-    prior = pm.sample_prior_predictive()
-    # step = pm.Metropolis()
-    # trace = pm.sample(2000, tune=1000, target_accept=0.9, cores=1, step=step)
-    trace = pm.sample(2000, tune=1000, target_accept=0.9, cores=1)
-    posterior_predictive = pm.sample_posterior_predictive(trace)
-
-    data = az.from_pymc3(trace=trace, prior = prior, posterior_predictive = posterior_predictive)
-
-# pm.traceplot(trace);
+pm.traceplot(trace);
 pm.energyplot(trace);
 az.plot_posterior(data, round_to=2, credible_interval=0.95);
+pm.summary(trace, var_names=['R0']).round(0)
+pm.forestplot(trace, var_names=['R0'])
+
+y_n = data.observed_data['Y']
+plt.plot(times[1::], y_n[:, 0], color='C0', alpha=0.5, label=f'$S(t)$')
+plt.plot(times[1::], y_n[:, 1], color='C1', alpha=0.5, label=f'$I(t)$')
+plt.plot(times[1::], y_n[:, 2], color='C2', alpha=0.5, label=f'$R(t)$')
+plt.legend()
+plt.title(f'R0={beta/gamma:.2f}; $\\beta$={beta}; $\\gamma$={gamma:.2f}; d={14}')
+plt.show()
+
+
+y_n = data.post ['Y']
+plt.plot(times[1::], y_n[:, 0], color='C0', alpha=0.5, label=f'$S(t)$')
+plt.plot(times[1::], y_n[:, 1], color='C1', alpha=0.5, label=f'$I(t)$')
+plt.plot(times[1::], y_n[:, 2], color='C2', alpha=0.5, label=f'$R(t)$')
+plt.legend()
+plt.title(f'R0={beta/gamma:.2f}; $\\beta$={beta}; $\\gamma$={gamma:.2f}; d={14}')
+plt.show()
--- SimCode/SEIR_MCMC/temp.py
+++ SimCode/SEIR_MCMC/temp.py
@@ -21,9 +21,9 @@ y = odeint(SIR, t=times, y0=[0.99, 0.01], args=((beta,gamma),), rtol=1e-8)
 yobs = np.random.lognormal(mean=np.log(y[1::]), sigma=[0.2, 0.3])
 
 
-plt.plot(times[1::],yobs, marker='o', linestyle='none')
-plt.plot(times, y[:,0], color='C0', alpha=0.5, label=f'$S(t)$')
-plt.plot(times, y[:,1], color ='C1', alpha=0.5, label=f'$I(t)$')
+plt.plot(times[1::], yobs, marker='o', linestyle='none')
+plt.plot(times, y[:, 0], color='C0', alpha=0.5, label=f'$S(t)$')
+plt.plot(times, y[:, 1], color ='C1', alpha=0.5, label=f'$I(t)$')
 plt.legend()
 plt.show()
 
@@ -40,8 +40,8 @@ with pm.Model() as model4:
     sigma = pm.HalfCauchy('sigma', 1, shape=2)
 
     # R0 is bounded below by 1 because we see an epidemic has occured
-    R0 = pm.Bound(pm.Normal, lower=1)('R0', 2,3)
-    lam = pm.Lognormal('lambda',pm.math.log(2), 2)
+    R0 = pm.Bound(pm.Normal, lower=1)('R0', 2, 3)
+    lam = pm.Lognormal('lambda', pm.math.log(2), 2)
     beta = pm.Deterministic('beta', lam*R0)
 
     sir_curves = sir_model(y0=[0.99, 0.01], theta=[beta, lam])
@@ -49,7 +49,9 @@ with pm.Model() as model4:
     Y = pm.Lognormal('Y', mu=pm.math.log(sir_curves), sd=sigma, observed=yobs)
 
     prior = pm.sample_prior_predictive()
-    trace = pm.sample(2000,tune=1000, target_accept=0.9, cores=1)
+    # step = pm.Metropolis()
+    # trace = pm.sample(2000, tune=1000, target_accept=0.9, cores=1, step=step)
+    trace = pm.sample(2000, tune=1000, nuts_kwargs={"target_accept": 0.9},  cores=1, start={'lamda':4.0, 'beta':4.0})
     posterior_predictive = pm.sample_posterior_predictive(trace)
 
     data = az.from_pymc3(trace=trace, prior = prior, posterior_predictive = posterior_predictive)
