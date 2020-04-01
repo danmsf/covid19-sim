2a04a2210e866ccb476d7a55430c671a7624529d
Dan <d@d.com>
2020-04-02 02:11:30 +0300

    add SEIRSPlus model

--- SimCode/src/app.py
+++ SimCode/src/app.py
@@ -189,39 +189,40 @@ if "SEIRSPlus" in models_option:
     st.markdown(
         """*Graph generated from `SEIRSPlus` package*"""
     )
-    st.markdown(
-        """### Model Parameters"""
-    )
-    st.markdown(
-        """
-        
-        Constructor Argument | Parameter Description | Data Type | Default Value
-        -----|-----|-----|-----
-        ```beta   ``` | rate of transmission | float | REQUIRED
-        ```sigma  ``` | rate of progression | float | REQUIRED
-        ```gamma  ``` | rate of recovery | float | REQUIRED
-        ```xi     ``` | rate of re-susceptibility | float | 0
-        ```mu_I   ``` | rate of infection-related mortality | float | 0
-        ```mu_0   ``` | rate of baseline mortality | float | 0 
-        ```nu     ``` | rate of baseline birth | float | 0 
-        ```beta_D ``` | rate of transmission for detected cases | float | None (set equal to ```beta```) 
-        ```sigma_D``` | rate of progression for detected cases | float | None (set equal to ```sigma```)  
-        ```gamma_D``` | rate of recovery for detected cases | float | None (set equal to ```gamma```)  
-        ```mu_D   ``` | rate of infection-related mortality for detected cases | float | None (set equal to ```mu_I```) 
-        ```theta_E``` | rate of testing for exposed individuals | float | 0 
-        ```theta_I``` | rate of testing for infectious individuals | float | 0 
-        ```psi_E  ``` | probability of positive tests for exposed individuals | float | 0 
-        ```psi_I  ``` | probability of positive tests for infectious individuals | float | 0
-        ```initN  ``` | initial total number of individuals | int | 10
-        ```initI  ``` | initial number of infectious individuals | int | 10
-        ```initE  ``` | initial number of exposed individuals | int | 0 
-        ```initD_E``` | initial number of detected exposed individuals | int | 0 
-        ```initD_I``` | initial number of detected infectious individuals | int | 0 
-        ```initR  ``` | initial number of recovered individuals | int | 0
-        ```initF  ``` | initial number of deceased individuals | int | 0
-
-        """
-    )
+    if st.checkbox(label="Model Parameters", value=False):
+        st.markdown(
+            """### Model Parameters"""
+        )
+        st.markdown(
+            """
+            
+            Constructor Argument | Parameter Description | Data Type | Default Value
+            -----|-----|-----|-----
+            ```beta   ``` | rate of transmission | float | REQUIRED
+            ```sigma  ``` | rate of progression | float | REQUIRED
+            ```gamma  ``` | rate of recovery | float | REQUIRED
+            ```xi     ``` | rate of re-susceptibility | float | 0
+            ```mu_I   ``` | rate of infection-related mortality | float | 0
+            ```mu_0   ``` | rate of baseline mortality | float | 0 
+            ```nu     ``` | rate of baseline birth | float | 0 
+            ```beta_D ``` | rate of transmission for detected cases | float | None (set equal to ```beta```) 
+            ```sigma_D``` | rate of progression for detected cases | float | None (set equal to ```sigma```)  
+            ```gamma_D``` | rate of recovery for detected cases | float | None (set equal to ```gamma```)  
+            ```mu_D   ``` | rate of infection-related mortality for detected cases | float | None (set equal to ```mu_I```) 
+            ```theta_E``` | rate of testing for exposed individuals | float | 0 
+            ```theta_I``` | rate of testing for infectious individuals | float | 0 
+            ```psi_E  ``` | probability of positive tests for exposed individuals | float | 0 
+            ```psi_I  ``` | probability of positive tests for infectious individuals | float | 0
+            ```initN  ``` | initial total number of individuals | int | 10
+            ```initI  ``` | initial number of infectious individuals | int | 10
+            ```initE  ``` | initial number of exposed individuals | int | 0 
+            ```initD_E``` | initial number of detected exposed individuals | int | 0 
+            ```initD_I``` | initial number of detected infectious individuals | int | 0 
+            ```initR  ``` | initial number of recovered individuals | int | 0
+            ```initF  ``` | initial number of deceased individuals | int | 0
+    
+            """
+        )
     if st.checkbox(label="Detailed information on model", value=False):
         # TODO: Fix images
         st.markdown(
