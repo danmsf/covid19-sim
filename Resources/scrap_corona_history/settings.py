5f4464249de209ce14f6cd00cd0dcbb9586e7350
royassis <royassisg@gmail.com>
2020-04-02 19:25:51 +0300

    added my code to the proj

--- Resources/scrap_corona_history/settings.py
+++ Resources/scrap_corona_history/settings.py
@@ -1,8 +1,6 @@
 import os
-from collections import namedtuple
 import pandas as pd
 from selenium import webdriver
-import json
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
