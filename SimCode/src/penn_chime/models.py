3aecc1176d2c2db3533ce671ebeb122f15fafab3
gstat-gcloud <dan.feldman@g-stat.com>
2020-04-05 12:05:03 +0300

    todo

--- SimCode/src/penn_chime/models.py
+++ SimCode/src/penn_chime/models.py
@@ -247,6 +247,7 @@ class Seiar:
         self.results = pd.concat(results, axis = 1)
         return 0
 
+
 class OLG:
     def __init__(self, p: Parameters):
         self.periods_count = len(p.daily_hospitalized)
