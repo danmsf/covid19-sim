ce5b26f35c7beb1b856a1626d676206c5f250ff2
royassis <royassisg@gmail.com>
2020-04-07 15:30:05 +0300

    updated corona scraping

--- scrap_corona_history/snippets/Get_gov_data.py
+++ scrap_corona_history/snippets/Get_gov_data.py
@@ -9,17 +9,18 @@ OUTDIR = 'data/gov_data'
 RECORDS_LIMIT = 10000000
 
 GOVSITE_BASE_URL = "https://data.gov.il/api/action/datastore_search?"
+
 RESOURCE_ID1= "9eedd26c-019b-433a-b28b-efcc98de378d"
 RESOURCE_1_NAME = 'isolations'
 RESOURCE_ID2= "dcf999c1-d394-4b57-a5e0-9d014a62e046"
 RESOURCE_2_NAME = 'lab_tests'
 
-QUERY1 = f"{GOVSITE_BASE_URL}resource_id={RESOURCE_ID1}&limit={RECORDS_LIMIT}"
-QUERY2 = f"{GOVSITE_BASE_URL}resource_id={RESOURCE_ID2}&limit={RECORDS_LIMIT}"
+query1 = f"{GOVSITE_BASE_URL}resource_id={RESOURCE_ID1}&limit={RECORDS_LIMIT}"
+query2 = f"{GOVSITE_BASE_URL}resource_id={RESOURCE_ID2}&limit={RECORDS_LIMIT}"
 
 DFholder = namedtuple("DFholder", "link name")
-holder_labs = DFholder(QUERY1, RESOURCE_1_NAME)
-holder_isolations = DFholder(QUERY2, RESOURCE_2_NAME)
+holder_labs = DFholder(query1, RESOURCE_1_NAME)
+holder_isolations = DFholder(query2, RESOURCE_2_NAME)
 
 holders = [holder_labs,holder_isolations]
 
