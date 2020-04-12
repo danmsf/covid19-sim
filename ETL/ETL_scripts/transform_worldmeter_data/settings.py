e6566651f10c8b36d20f013021c7146cef5c9fe1
royassis <royassisg@gmail.com>
2020-04-12 12:51:09 +0300

    changed proj struct

---
+++ ETL/ETL_scripts/transform_worldmeter_data/settings.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+current_dir = os.path.dirname(__file__)
+
+# Paths and Dirs
+WORLDMETER_DATA = os.path.join(current_dir,'DW/raw_data/worldmeter')
+GOV_RESOURCE_CSV = os.path.join(current_dir, 'resources/csv/gov_resource.csv')
+
+MAPPER_PATH = os.path.join(current_dir,'resources/csv/column_remapper.csv')
+mapper = pd.read_csv(MAPPER_PATH, index_col ='key', usecols = ['key', 'value'])
+column_remapper = mapper.iloc[:,0]
+
+GOVERNMENT_RESPONSE_URL = 'https://ocgptweb.azurewebsites.net/CSVDownload'
+
+POPULATION_CSV_PATH = os.path.join(current_dir,'resources/csv/population_data.csv')
+
+GOV_DATA_RESULTS_DIR = os.path.join(current_dir, 'DW/raw_data/gov_data')
+
+RESULTS_PATH = os.path.join(GOV_DATA_RESULTS_DIR, 'all_dates.csv')
+RESULTS_PATH_SEIR = os.path.join(GOV_DATA_RESULTS_DIR, 'all_dates_seir.csv')
+
+GOV_DATA_PATH = os.path.join(current_dir, 'DW/raw_data/gov_data')
--- ETL/sample_script.py
+++ ETL/sample_script.py
@@ -1,6 +1,6 @@
 from ETL.ETL_scripts.extract_worldmeter.extract_worldmeter_data import main as extract_worldmeter_data
-from ETL.ETL_scripts.transform_worldmeter_data import main as transform_worldmeter_data
-from ETL.ETL_scripts.extract_gov_data import main as extract_gov_data
+from ETL.ETL_scripts.transform_worldmeter_data.transform_worldmeter_data import main as transform_worldmeter_data
+from ETL.ETL_scripts.extract_gov_data.extract_gov_data import main as extract_gov_data
 from ETL.ETL_scripts.extract_gsheets.covid19sheets import main as extract_sheet_data
 
 results_dir = '../Resources'
