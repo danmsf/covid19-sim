9468b0797578f3a0590689cefaf1700828653a67
royassis <royassisg@gmail.com>
2020-04-12 15:17:04 +0300

    cleanup

--- ETL/ETL_scripts/extract_regular_csvs/settings.py
+++ ETL/ETL_scripts/extract_regular_csvs/settings.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 URLS_PATH = 'resources/csv/urls.csv'
-URLS_PATH = r'D:\PycharmProjects\covid19-sim\ETL\ETL_scripts\extract_regular_csvs\resources\csv\urls.csv'
-
 urls = pd.read_csv(URLS_PATH)
 
 
