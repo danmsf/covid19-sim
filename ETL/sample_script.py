c9984530609f9c8bc999c66e50a4317001245dfb
royassis <royassisg@gmail.com>
2020-04-12 11:49:44 +0300

    fixed paths

--- ETL/sample_script.py
+++ ETL/sample_script.py
@@ -2,12 +2,12 @@ from ETL.ETL_scripts.extract_worldmeter.extract_worldmeter_data import main as e
 from ETL.ETL_scripts.transform_worldmeter_data import main as transform_worldmeter_data
 from ETL.ETL_scripts.extract_gov_data import main as extract_gov_data
 from ETL.ETL_scripts.extract_gsheets.covid19sheets import main as extract_sheet_data
-import os
+import pathlib
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-worldmeter_dir = os.path.join(current_dir,'DW/raw_data/worldmeter')
-results_dir = os.path.join(current_dir,'DW/loaded_data')
-gov_dir = os.path.join(current_dir,'DW/raw_data/gov_data')
+
+worldmeter_dir = pathlib.Path(__file__).parent.joinpath('DW/raw_data/worldmeter').__str__()
+results_dir = pathlib.Path(__file__).parent.parent.joinpath('Resources/Datasets').__str__()
+gov_dir = pathlib.Path(__file__).parent.parent.joinpath('Resources/Datasets/IsraelData').__str__()
 
 extract_worldmeter_data(worldmeter_dir)
 transform_worldmeter_data(worldmeter_dir,
@@ -15,4 +15,3 @@ transform_worldmeter_data(worldmeter_dir,
                           cutoffdate = '2020-02-10')
 extract_gov_data(gov_dir)
 extract_sheet_data(gov_dir)
-
