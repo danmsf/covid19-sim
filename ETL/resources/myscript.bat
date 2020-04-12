09dcb880e255200afff7d5d1f9d00e8aa364e981
royassis <royassisg@gmail.com>
2020-04-12 11:58:25 +0300

    updated my .bat script

--- ETL/resources/myscript.bat
+++ ETL/resources/myscript.bat
@@ -1,6 +1,3 @@
 call D:\Anaconda3\Scripts\activate.bat D:\Anaconda3
 D:
-cd "D:\PycharmProjects\scrap_corona_history"
-python "extract_worldmeter_data.py"
-python "transform_worldmeter_data.py"
-python "extract_gov_data.py"
+cd "D:\PycharmProjects\covid19-sim\ETL\sample_script.py"
