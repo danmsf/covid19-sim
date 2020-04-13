7a46b687497910c8d62335f7760784b0ce21be62
gstat-gcloud <dan.feldman@g-stat.com>
2020-04-13 21:51:23 +0300

    fix JH

--- SimCode/src/penn_chime/charts.py
+++ SimCode/src/penn_chime/charts.py
@@ -391,12 +391,12 @@ def olg_projections_chart(alt, df: pd.DataFrame, title: str, baseline=False):
     # Draw text labels near the points, and highlight based on selection
     text = line1.mark_text(align='left', dx=5, dy=-5).encode(
         text=alt.condition(nearest, 'value', alt.value(' ')),
-        y=alt.Y('value', axis=alt.Axis(labels=False, title='', tickOpacity=0)),
+        y=alt.Y('value', axis=alt.Axis(labels=True, title='', tickOpacity=0)),
     )
 
     text2 = line2.mark_text(align='left', dx=5, dy=-5).encode(
         text=alt.condition(nearest, 'value', alt.value(' ')),
-        y=alt.Y('value', axis=alt.Axis(labels=False, title='', tickOpacity=0)),
+        y=alt.Y('value', axis=alt.Axis(labels=True, title='', tickOpacity=0)),
     )
 
     # Draw a rule at the location of the selection
