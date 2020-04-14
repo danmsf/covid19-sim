5eadac9e2b1e5cd68f59d8e83ed25bb015de2d22
royassis <royassisg@gmail.com>
2020-04-14 20:31:28 +0300

    changes

---
+++ ETL/ETL_scripts/__init__.py
@@ -0,0 +1,5 @@
+from ETL_scripts.extract_worldmeter.extract_worldmeter_data import main as extract_worldmeter_data
+from ETL_scripts.transform_worldmeter_data.transform_worldmeter_data import main as transform_worldmeter_data
+from ETL_scripts.extract_gov_data.extract_gov_data import main as extract_gov_data
+from ETL_scripts.extract_gsheets.covid19sheets import main as extract_sheet_data
+from ETL_scripts.extract_regular_csvs.main import main as extract_regular_csvs
--- ETL/sample_script.py
+++ ETL/sample_script.py
@@ -1,14 +1,14 @@
-import ETL
+import ETL_scripts
 
 country_data_dir = '../Resources/Datasets/CountryData'
 worldmeter_data_dir = 'DW/raw_data/worldmeter'
 israel_data_dir = '../Resources/Datasets/IsraelData'
 
-ETL.extract_worldmeter_data(outdir = worldmeter_data_dir)
-ETL.transform_worldmeter_data(indir= worldmeter_data_dir,
-                          outdir= country_data_dir,
-                          cutoffdate = '2020-02-10')
-ETL.extract_gov_data(outdir=israel_data_dir)
-ETL.extract_sheet_data(outdir=israel_data_dir)
-ETL.extract_regular_csvs(outdir=country_data_dir)
+ETL_scripts.extract_worldmeter_data(outdir = worldmeter_data_dir)
+ETL_scripts.transform_worldmeter_data(indir= worldmeter_data_dir,
+                                      outdir= country_data_dir,
+                                      cutoffdate = '2020-02-10')
+ETL_scripts.extract_gov_data(outdir=israel_data_dir)
+ETL_scripts.extract_sheet_data(outdir=israel_data_dir)
+ETL_scripts.extract_regular_csvs(outdir=country_data_dir)
 
