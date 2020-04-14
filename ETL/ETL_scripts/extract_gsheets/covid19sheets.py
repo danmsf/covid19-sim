86ce4f9b67dfdce591323ce7aa968066b7032915
royassis <royassisg@gmail.com>
2020-04-14 16:13:10 +0300

    for gsheets getter changed func signature added typing

--- ETL/ETL_scripts/extract_gsheets/covid19sheets.py
+++ ETL/ETL_scripts/extract_gsheets/covid19sheets.py
@@ -1,4 +1,6 @@
 from .utils.functions import *
+from .settings import *
+from typing import IO
 
 # Enable sheets api at: https://developers.google.com/sheets/api/quickstart/python
 # Choose desktop app
@@ -14,10 +16,10 @@ SHEET_NAME = 'Patients/towns'
 RANGE = 'A3:AB'
 SAMPLE_RANGE_NAME = f'{SHEET_NAME}!{RANGE}'
 
-def main(outdir = None):
+def main(outdir:IO = None)->pd.DataFrame:
     print(__file__, 'is running')
 
-    service = create_service(SCOPES)
+    service = create_service(SCOPES, CREDS_PATH, TOKEN_PATH)
     # Call the Sheets API
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
