4ac859b9146beb2be563c955ed6ddfebd238fe8f
royassis <royassisg@gmail.com>
2020-04-19 19:50:57 +0300

    fixed data manipulation

--- ETL/sample_script.py
+++ ETL/sample_script.py
@@ -4,11 +4,11 @@ country_data_dir = '../Resources/Datasets/CountryData'
 worldmeter_data_dir = 'DW/raw_data/worldmeter'
 israel_data_dir = '../Resources/Datasets/IsraelData'
 
-ETL_scripts.extract_worldmeter_data(outdir = worldmeter_data_dir)
-ETL_scripts.transform_worldmeter_data(indir= worldmeter_data_dir,
-                                      outdir= country_data_dir,
-                                      cutoffdate = '2020-02-10')
-ETL_scripts.extract_gov_data(outdir=israel_data_dir)
+# ETL_scripts.extract_worldmeter_data(outdir = worldmeter_data_dir)
+# ETL_scripts.transform_worldmeter_data(indir= worldmeter_data_dir,
+#                                       outdir= country_data_dir,
+#                                       cutoffdate = '2020-02-10')
+# ETL_scripts.extract_gov_data(outdir=israel_data_dir)
 ETL_scripts.extract_sheet_data(outdir=israel_data_dir)
-ETL_scripts.extract_regular_csvs(outdir=country_data_dir)
+# ETL_scripts.extract_regular_csvs(outdir=country_data_dir)
 
