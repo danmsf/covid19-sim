9b89b5f3c77fc5855d82f78d45858ad0f3337659
royassis <royassisg@gmail.com>
2020-04-19 15:42:02 +0300

    fixed data manipulation

--- ETL/ETL_scripts/transform_worldmeter_data/transform_worldmeter_data.py
+++ ETL/ETL_scripts/transform_worldmeter_data/transform_worldmeter_data.py
@@ -42,8 +42,9 @@ def main(indir:IO,
 
     # Join df from all dates
     disease_data = pd.concat(df_list, ignore_index=True, sort=False)
-    # Remove plus sign from "New Cases" col
+    # Remove plus sign from columns
     disease_data['New Cases'] = disease_data['New Cases'].str.replace('[^\d]','')
+    disease_data['New Deaths'] = disease_data['New Deaths'].str.replace('[^\d]', '')
     # Sort df by date
     disease_data = disease_data.sort_values('date')
     # Remove the totalrow
