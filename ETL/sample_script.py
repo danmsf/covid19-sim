2bb8110ca0488fb4e085bb21e4f85afdbe819898
gstat-gcloud <dan.feldman@g-stat.com>
2020-04-26 23:31:48 +0300

    adapt ETL code to new beginning

--- ETL/sample_script.py
+++ ETL/sample_script.py
@@ -7,7 +7,7 @@ worldmeter_data_dir = 'DW/raw_data/worldmeter'
 israel_data_dir = '../Resources/Datasets/IsraelData'
 worldmeter_data_dir1 = os.path.join(os.getcwd(), worldmeter_data_dir)
 
-# ETL_scripts.extract_worldmeter_data(outdir=worldmeter_data_dir)
+ETL_scripts.extract_worldmeter_data(outdir=worldmeter_data_dir)
 ETL_scripts.transform_worldmeter_data(indir=worldmeter_data_dir1,
                                       outdir=country_data_dir,
                                       cutoffdate='2020-02-10')
