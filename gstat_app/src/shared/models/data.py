bee2691811010b490d1e6e76ef7f701b6fb5f56e
gstat-gcloud <dan.feldman@g-stat.com>
2020-04-27 12:10:45 +0300

    test new app

--- gstat_app/src/shared/models/data.py
+++ gstat_app/src/shared/models/data.py
@@ -11,6 +11,7 @@ class CountryData:
         self.sir_df = self.get_sir()
         self.jh_confirmed_df = self.get_jhopkins_confirmed()
 
+    @st.cache
     def get_country_data(self):
         country_df = pd.read_csv(self.country_files['country_file'])
         # country_df = country_df.set_index('Country')
@@ -25,15 +26,18 @@ class CountryData:
         # country_df['new_deaths'] = country_df['new_deaths'].apply(lambda x: float(x))
         return country_df
 
+    @st.cache
     def get_country_stringency(self):
         country_st_df = pd.read_excel(self.country_files['stringency_file'])
         country_st_df['date'] = pd.to_datetime(country_st_df['Date'], format="%Y%m%d")
         return country_st_df
 
+    @st.cache
     def get_data(self):
         df = self.country_df.merge(self.stringency_df, left_on=["Country", "date"], right_on=["CountryName", "date"])
         return df
 
+    @st.cache
     def get_sir(self):
         sir_df = pd.read_csv(self.country_files['sir_file'])
         # sir_df = sir_df.set_index('Country')
@@ -45,6 +49,7 @@ class CountryData:
         sir_df = sir_df.rename(columns={'country': 'Country'})
         return sir_df
 
+    @st.cache
     def get_jhopkins_confirmed(self):
         df = pd.read_csv(self.country_files['jhopkins_confirmed'])
         # df = df.drop(columns="Unnamed: 0")
@@ -67,7 +72,7 @@ class IsraelData:
         self.tested_df = self.get_tested_df()
         self.patients_df = self.get_patients_df()
 
-    # @st.cache
+    @st.cache
     def get_yishuv_data(self):
         df = pd.read_csv(self.filepath['yishuv_file'])
         df = df.drop(columns="Unnamed: 0")
@@ -87,7 +92,7 @@ class IsraelData:
         df = df.rename(columns={'יישוב': 'Yishuv'})
         return df
 
-    # @st.cache
+    @st.cache
     def get_isolation_df(self):
         df = pd.read_csv(self.filepath['isolations_file'])
         df['date'] = pd.to_datetime(df['date'])
@@ -95,13 +100,13 @@ class IsraelData:
         df = df.drop(columns="_id")
         return df
 
-    # @st.cache
+    @st.cache
     def get_lab_results_df(self):
         df = pd.read_csv(self.filepath['lab_results_file'])
         df['result_date'] = pd.to_datetime(df['result_date'])
         return df
 
-    # @st.cache
+    @st.cache
     def get_tested_df(self):
         df = pd.read_csv(self.filepath['tested_file'])
         df['None'] = df[df.columns[2:]].sum(axis=1)
@@ -111,7 +116,7 @@ class IsraelData:
         df = df.drop(columns="_id")
         return df
 
-    # @st.cache
+    @st.cache
     def get_patients_df(self):
         df = pd.read_csv(self.filepath['patients_file'])
         df = df.dropna(subset=['New Patients Amount'])
