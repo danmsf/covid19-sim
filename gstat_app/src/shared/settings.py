9008c4ac9122df1e09f93fc55b2ce2d4d2e18fef
gstat-gcloud <dan.feldman@g-stat.com>
2020-04-27 17:52:36 +0300

    testing fancy cache

--- gstat_app/src/shared/settings.py
+++ gstat_app/src/shared/settings.py
@@ -23,7 +23,7 @@ with open(defaults_file) as file:
 user_session_id = get_session_id()
 print(user_session_id)
 # @fancy_cache(ttl=10, unique_to_session=False)
-@st.cache(ttl=10, show_spinner=True, max_entries=2)
+@st.cache(ttl=10, show_spinner=True,persist=True, max_entries=2)
 def load_data(DEFAULTS,user_session_id):
     print(user_session_id)
     israel_data = IsraelData(DEFAULTS['FILES']['israel_files'])
