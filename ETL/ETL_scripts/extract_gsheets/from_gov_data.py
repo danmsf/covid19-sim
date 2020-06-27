948044192e6dba39fc21990555428e165207b42e
gstat-gcloud <dan.feldman@g-stat.com>
2020-06-27 22:30:30 +0300

    update data

--- ETL/ETL_scripts/extract_gsheets/from_gov_data.py
+++ ETL/ETL_scripts/extract_gsheets/from_gov_data.py
@@ -40,6 +40,8 @@ files = ["כלל הארץ לשליחה 26.04.20 שעה 09.00.xlsx",
          "דוח_אקסל_חדש_כלל_הארץ_כולל_מועצות_אזוריות_23_06_20_שעה_19_30.xlsx",
          "דוח_אקסל_חדש_כלל_הארץ_כולל_מועצות_אזוריות_24_06_20_שעה_10_30.xlsx",
          "דוח_אקסל_חדש_כלל_הארץ_כולל_מועצות_אזוריות_25_06_20_שעה_10_30.xlsx",
+         "דוח_אקסל_חדש_כלל_הארץ_כולל_מועצות_אזוריות_26_06_20_שעה_18_00.xlsx",
+         "דוח_אקסל_חדש_כלל_הארץ_כולל_מועצות_אזוריות_27_06_20_שעה_21_30.xlsx"
          ]
 
 dates = ['20200508',
@@ -69,6 +71,8 @@ dates = ['20200508',
          '20200623',
          '20200624',
          '20200625',
+         '20200626',
+         '20200627',
          ]
 
 file = files[-1]
@@ -100,16 +104,14 @@ t.to_csv(path_out + 'yishuv_' + dt + ".csv", index=False)
 
 # -----------------------------Join files------------------------
 
-
-p23 = pd.read_csv(path_out + 'yishuv_' + '20200623' + ".csv")
-p24 = pd.read_csv(path_out + 'yishuv_' + '20200624' + ".csv")
-p25 = pd.read_csv(path_out + 'yishuv_' + '20200625' + ".csv")
-joined = pd.concat([p23, p24, p25])
+p26 = pd.read_csv(path_out + 'yishuv_' + '20200626' + ".csv")
+p27 = pd.read_csv(path_out + 'yishuv_' + '20200627' + ".csv")
+joined = pd.concat([p26, p27])
 joined = joined.dropna(subset=['יישוב', 'pop2018'])
 
 yishuv_file = pd.read_csv(path_out + 'yishuv_file.csv')
 yishuv_file = pd.concat([yishuv_file, joined])
-yishuv_file['last_updated'] = pd.to_datetime('20200625')
+yishuv_file['last_updated'] = pd.to_datetime('20200627')
 yishuv_file['date'] = pd.to_datetime(yishuv_file['date']).dt.date
 yishuv_file.to_csv(path_out + 'yishuv_file.csv', index=False)
 
